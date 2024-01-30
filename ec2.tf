e2a8fd6f6582b763af3d4fbe2c0db0b3c8839f69
Frida <frida>
2024-01-30 00:09:34 -0500

    code

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "server1" {
     instance_type = var.instance_type
- ami = "ami-00b8917ae86a424c9"
+ ami = data.aws_ami1.id
  tags ={
-    Name = "terraform-server1"
+    Name = "Dev-server"
  }
 
 
